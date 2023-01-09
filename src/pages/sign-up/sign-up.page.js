c7b784669908169fa0106c46e6d2c091dd2c8523
Jonas Peres <jonasperes10@hotmail.com>
2023-01-09 12:48:33 -0300

    Update Sign Up Page

--- src/pages/sign-up/sign-up.page.js
+++ src/pages/sign-up/sign-up.page.js
@@ -1,9 +1,21 @@
+import Input from "../../components/input/input.component";
+import Button from "../../components/button/button.component";
+
 const SignUp = () => {
   return (
     <div>
-      <h1>Sign Up</h1>
-      <input />
-      <input />
+      <form onSubmit={null}>
+        <h1>Sign Up</h1>
+        <Input label="User" name="user-name-input" id="user-name-input" />
+        <Input label="E-mail" name="email-input" id="email-input" />
+        <Input label="Password" name="password-input" id="password-input" />
+        <Input
+          label="Confirm Password"
+          name="confirm-password-input"
+          id="confirm-password-input"
+        />
+        <Button />
+      </form>
     </div>
   );
 };
