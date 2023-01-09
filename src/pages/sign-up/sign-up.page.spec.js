f09d9c79cb03193c6093fb0845cca28ba7233682
Jonas Peres <jonasperes10@hotmail.com>
2023-01-09 13:06:53 -0300

    Sign Up Test

--- src/pages/sign-up/sign-up.page.spec.js
+++ src/pages/sign-up/sign-up.page.spec.js
@@ -5,13 +5,36 @@ describe("Sign Up Page", () => {
   describe("Layout", () => {
     it("Has Header", () => {
       render(<SignUp />);
-      const header = screen.queryByRole("heading", { name: "Sign Up" });
+      const header = screen.getByRole("heading", {
+        level: 1,
+        name: "Sign Up",
+      });
       expect(header).toBeInTheDocument();
     });
     it("Has UserName Input", () => {
       render(<SignUp />);
-      const inputs = screen.getAllByRole("textbox");
-      expect(inputs.length).toBe(2);
+      const input = screen.getByLabelText("User");
+      expect(input).toBeInTheDocument();
+    });
+    it("Has E-mail Input", () => {
+      render(<SignUp />);
+      const input = screen.getByLabelText("E-mail");
+      expect(input).toBeInTheDocument();
+    });
+    it("Has Password Input", () => {
+      render(<SignUp />);
+      const input = screen.getByLabelText("Password");
+      expect(input).toBeInTheDocument();
+    });
+    it("Has Password Confirm Input", () => {
+      render(<SignUp />);
+      const input = screen.getByLabelText("Confirm Password");
+      expect(input).toBeInTheDocument();
+    });
+    it("Has Confirm Button", () => {
+      render(<SignUp />);
+      const button = screen.getByRole("button", { type: "submit" });
+      expect(button).toBeInTheDocument();
     });
   });
 });
