6dbc0ff6ebb81e933c2450f416a05a25ac47f75c
Jonas Peres <jonasperes10@hotmail.com>
2023-03-09 11:00:16 -0300

    add react-toastify

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ToastContainer } from "react-toastify";
 import App from "./App";
 import "./index.css";
 
@@ -7,5 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <ToastContainer position="top-center" theme="colored" draggable={false} />
   </React.StrictMode>
 );
